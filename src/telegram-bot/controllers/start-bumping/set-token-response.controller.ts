fcede64374bf976dd9c231676b0313f34439806b
Stelios Mavrokoukoulakis <stylianos.mavrokoukoulakis@gmail.com>
2024-11-26 18:29:21 +0200

    Added logs to test why not bumping

--- src/telegram-bot/controllers/start-bumping/set-token-response.controller.ts
+++ src/telegram-bot/controllers/start-bumping/set-token-response.controller.ts
@@ -23,12 +23,16 @@ export async function setTokenResponseController({
   const { from } = message;
   if (!message || !from) return;
 
+  console.log("Message: ", message);
+
   // Initialize services
   const userService = new UserService();
 
   // Get the user
   const user = await userService.getUser(from.id);
   if (!user) return;
+
+  console.log("User: ", user);
 
   // Start loading
   const sentLoading = await loadingController({
@@ -39,6 +43,8 @@ export async function setTokenResponseController({
     setUserState,
   });
   const loadingMsgId = sentLoading?.message_id;
+
+  console.log("Sent loading: ", sentLoading);
 
   const pumpFunService = new PumpFunService();
   const solanaService = new SolanaService();
@@ -53,6 +59,14 @@ export async function setTokenResponseController({
 
   // Validate coin data result
   if (!coinData) {
+    // Stop loading
+    await loadingController({
+      bot,
+      message,
+      msgId: loadingMsgId,
+      getUserState,
+      setUserState,
+    });
     errorController({
       bot,
       message,
@@ -67,6 +81,8 @@ export async function setTokenResponseController({
   const userState = getUserState();
   setUserState!({ ...userState!, lastCallback: null });
 
+  console.log("User state: ", userState);
+
   // const sufficientBalance = await pumpFunService.hasSufficientBalance(ca);
   const { totalRequiredBalance, payerBalance } =
     await solanaService.getRequiredBalance(
@@ -79,14 +95,19 @@ export async function setTokenResponseController({
     );
   const hasSufficientBalance = payerBalance >= totalRequiredBalance;
 
+  console.log("Total required balance: ", totalRequiredBalance);
+  console.log("Payer's balance: ", payerBalance);
+
   // Stop loading
-  loadingController({
+  await loadingController({
     bot,
     message,
     msgId: loadingMsgId,
     getUserState,
     setUserState,
   });
+
+  console.log("Has sufficient balance: ", hasSufficientBalance);
 
   if (!hasSufficientBalance) {
     const errMsg = `*Insufficient balance.*
@@ -109,7 +130,7 @@ _Once done, press Refresh Balance to check your updated balance._`;
   }
 
   // Start bumping. Respond with the coin name and a "started bumping" message.
-  bot.sendMessage(
+  await bot.sendMessage(
     message.chat.id,
     `🔥  Started bumping meme coin: *${coinData.name}*  🔥
     
@@ -131,6 +152,8 @@ _Once done, press Refresh Balance to check your updated balance._`;
     }
   );
 
+  console.log("^^^^^ ", 1);
+
   // Start the bump interval
   const includeBotFee = getIncludeBotFeeForUser(user, coinData.mint);
   const bump = async () =>
@@ -150,6 +173,8 @@ _Once done, press Refresh Balance to check your updated balance._`;
     user.bumpsLimit,
     getUserState
   );
+
+  console.log("Bump response: ", bumpResponse);
 
   // Once the interval is done, the rest of the code will run
   if (bumpResponse.success) {
