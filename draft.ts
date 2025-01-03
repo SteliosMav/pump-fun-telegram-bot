49c6584dd12ad30cded1b8f3d28b5b9e9d312b32
Stelios Mavrokoukoulakis <stylianos.mavrokoukoulakis@gmail.com>
2025-01-03 15:10:07 +0200

    Stop tracking scripts and draft.ts

--- draft.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-// import "./scripts/testCoinData";
-// import "./scripts/updateDHCPSettings";
-// import "./scripts/update-user-profiles";
-// import "./scripts/news-letter";
-import "./scripts/test-user-model";
