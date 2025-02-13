ab6f2b0c9b4e889d5c9fd7dcefa77fb5c8f8420f
Stylianos Mavrokoukoulakis <stylianos.mavrokoukoulakis@gmail.com>
2025-02-13 22:29:01 +0200

    Added "to do" guidance for the bump method.

--- src/telegram-bot/home/home.service.ts
+++ src/telegram-bot/home/home.service.ts
@@ -14,12 +14,25 @@ export class HomeService {
 
   async bump(session: BotSessionData): Promise<void> {
     session.bumpStatus = BumpStatus.BUMPING;
-    const { bumpSettings: settings } = session.user;
+    const {
+      limit,
+      intervalInSeconds,
+      amountInSol,
+      slippage,
+      priorityFeeInSol,
+    } = session.user.bumpSettings;
 
     // === Start Interval ===
+    // A while look can be set here that sends requests without waiting for
+    // the previous request's response. If an error or two occurres in one of the
+    // requests, a boolean should be set that would prevent the loop from going on.
+    // Also that boolean should be affected from any user's interaction as well,
+    // that would cancel the bot's bumping.
     // ...
 
     // === Update User ===
+    // Update user in database as well as on the session object based on the
+    // response from the database.
     // ...
   }
 }
